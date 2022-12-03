a25904237018b67a2f50e763af7dc3d0518afb24
Krishan <111554369+krishanprajapat23@users.noreply.github.com>
2022-12-03 15:40:44 +0530

    fixed some minor issues

--- assets/scripts/dynamic.js
+++ assets/scripts/dynamic.js
@@ -1,105 +1,110 @@
-
 let localId = localStorage.getItem("movieId");
-// let localName = localStorage.getItem("moviename");
-// let localImg = localStorage.getItem("movieimg");
-// let localDesc = localStorage.getItem("storyline");
-// let localDate = localStorage.getItem("releasedate");
+const content = document.getElementById("content");
+
+//api url
+const info = `https://api.themoviedb.org/3/movie/${localId}?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`;
 
 
-
-const info =
-    `https://api.themoviedb.org/3/movie/${localId}?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`
+const vidUrl = `https://api.themoviedb.org/3/movie/${localId}/videos?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`;
 
 
-const vidUrl =
-    `https://api.themoviedb.org/3/movie/${localId}/videos?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`;
-
-
-async function getVid() {
-    const fetchData = await fetch(vidUrl).then((response) => {
-            //api call successful
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then((data) => {
-            //json from our response
-            console.log('-----', data.results);
-            //search reesult with name of search query
-            showVid(data.results);
-        })
-        .catch((err) => {
-            console.warn("data fetching failed", err);
-        });
-}
-
-function showVid(data) {
-    console.log(data)
-}
-
-showVid()
+// function to fetch api
 async function getInfo(url) {
-    const fetchData = await fetch(url).then((response) => {
-            //api call successful
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then((data) => {
-            //json from our response
-            console.log('-----', data.results);
-            //search reesult with name of search query
-            showMovies(data);
-        })
-        .catch((err) => {
-            console.warn("data fetching failed", err);
-        });
-
+  const fetchData = await fetch(url)
+    .then((response) => {
+      //api call successful
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .then((data) => {
+      //json from our response
+      showMovies(data);
+      showVid(data);
+    })
+    .catch((err) => {
+      console.warn("data fetching failed", err);
+    });
 }
-getInfo(info)
+getInfo(info);
 
-function showMovies(data) {
-    console.log(data)
-}
 
-function showMovies(data) {
-    data.forEach((movie) => {
-        // console.log(movie)
-        const {
-            poster_path,
-            release_date,
-            name,
-            id,
-            title,
-            vote_average,
-            overview,
-        } = movie;
-        console.log(movie, '+++++')
-        const movieEl = document.createElement("div");
-        movieEl.className = "movie col-md-4 col-lg-3 col-sm-6";
-
-        content.innerHTML = `
+// function to show data to dom
+function showMovies(movie) {
+//   console.log(movie);
+  //for dynamic page title
+  let titlePage = document.querySelector("title");
+  titlePage.textContent = movie?.title;
+  //=============================================================
+  //page rendering the movie details
+  const singleEl = document.createElement("div");
+  const allGenre = movie.genres.map((genre) => genre.name);
+  content.innerHTML = `
             <div class="row">
                 <div class="col-lg-6">
-                    <img class="poster-img" alt="" src="https://image.tmdb.org/t/p//w500"+${poster_path.trim()}" />
+                    <img class="poster-img" alt="" src="https://image.tmdb.org/t/p//w500/${movie?.poster_path.trim()}" />
                 </div>
                 <div class="col-lg-6">
                     <div class="content-main">
-                        <h1 class="movie-title fw-bold py-2">Title : ${localName}</h1>
-                        <p class="para fs-6 fw-normal"><strong>Released on</strong> : ${localDate}</p>
-                        <p class="para fs-6 fw-normal"><strong>Storyline</strong> : ${localDesc}</p>
+                        <h1 class="movie-title fw-bold py-2">${
+                          movie?.title
+                        }</h1>
+                        <p class="para mb-2 fs-6 fw-normal"><strong>Tagline</strong> : ${
+                          movie?.tagline
+                        }</p>
+                        <p class="para mb-2 fs-6 fw-normal"><strong>Status</strong> : ${
+                          movie?.status
+                        }</p>
+                        <p class="para mb-2 fs-6 fw-normal"><strong>Runtime</strong> : ${
+                          movie?.runtime
+                        } minutes</p>
+                        <p class="para mb-2 fs-6 fw-normal"><strong>Genre</strong> : ${allGenre}</p>
+                        <p class="para mb-2 fs-6 fw-normal"><strong>Released on</strong> : ${
+                          movie?.release_date
+                        }</p>
+                        <p class="para desc mb-2 fs-6 fw-normal"><strong>Storyline</strong> : ${
+                          movie?.overview
+                        }</p>
 
                     </div>
                 </div>
             </div>              
         `;
-        showContent.appendChild(movieEl);
-    });
+  content.appendChild(singleEl);
 }
 
 
-showMovies()
+// function to show youtube video
+ function showVid() {
+  async function getInfo() {
+    try {
+        const fetchData = await fetch(vidUrl)
+      .then((response) => {
+        //api call successful
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        let storeArrKey = data.results.map((x) => (x.type === "Trailer" ? x.key : null))
+          .filter((x) => x !== null);
+          console.log(storeArrKey[0]);
+          const ytVid = `https://www.youtube.com/embed/${storeArrKey[0]}`;
+          // console.log(ytVid);
+          const nearEl = document.querySelector(".content-main");
+          const iframe = document.createElement("iframe");
+          iframe.src = ytVid;
+          nearEl.parentNode.insertBefore(iframe, nearEl.nextSibling);
+
+      })
+    } catch (err) {
+        console.warn("data fetching failed", err);
+    }
+  }
+  getInfo();
+}
+
